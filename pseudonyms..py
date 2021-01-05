86c4f50d98d25e2d86e3925922d1401b470c7566
John-Vega <johnvega30@gmail.com>
2021-01-05 16:54:10 -0600

    Add files via upload

---
+++ pseudonyms..py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 20 21:51:33 2020
+
+@author: Johnny
+
+
+"""
+
+import sys
+import random
+
+print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+print("A name just like Sean would pick for Gus:\n\n")
+
+first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+             "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+             'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+             'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+             'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+             'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+             'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+             'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+             '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+             'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+             'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+             "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+             'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+             'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+             "Winston 'Jazz Hands'", 'Worms')
+
+last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
+
+middle = ('Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Roseworthy', 'Olivetti', 'Outerbridge', 'peck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Dinkerton', 'Porkins', 'Putney', 'Quakenbush', 'water',
+            'Rosenthal', 'Ruboff', 'rider', 'shine', 'Splern',
+            'Vtevens', 'Stroga')
+
+while True:
+    firstName = random.choice(first)
+    
+    middleName = random.choice(middle)
+    
+    lastName = random.choice(last)
+    
+    print("\n\n\n")
+    print("{}, {}, {}".format(firstName,middleName,lastName), file=sys.stderr)
+    
+    try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+    if try_again.lower() == "n":
+        break
+input("\nPress ENter to exit.")
